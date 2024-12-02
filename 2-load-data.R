2b33a193727eb6c3e5af7eb3b4c7f03a25928f2d
Arish Mudra Rakshasa-Loots <73881703+arishmr@users.noreply.github.com>
2024-12-02 18:44:13 +0000

    Update 2-load-data.R

--- 2-load-data.R
+++ 2-load-data.R
@@ -7,12 +7,12 @@ colnames(alldata)
 # convert variables to correct type
 
 alldata <- alldata %>% mutate_at(c(
-  "PID", "PHQ", "SADS", "age",
-  colnames(alldata[,12:20]),   ## PHQ9 scores
-  colnames(alldata[,21:36])
+  "PID", "PHQ", "SADS",
+  colnames(alldata[,9:17]),   ## PHQ9 scores
+  colnames(alldata[,18:33])
 ), as.numeric)
 
 alldata <- alldata %>% mutate_at(c(
-  "Group", "sex", "gender", "ethnicity", "curr.antidep", "ever.antidep"), as.factor)
+  "Group", "sex", "curr.antidep", "ever.antidep"), as.factor)
 
 alldata$Group <- relevel(alldata$Group, ref = "Low Depression Severity")
